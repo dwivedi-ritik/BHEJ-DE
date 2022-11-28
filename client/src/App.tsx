b11a4610c782f4939468643ab15c55b3c1040834
dwivedi-ritik <dwi.ritik20@gmail.com>
2022-11-28 22:52:41 +0530

    scoped

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,15 +1,23 @@
 import TopBar from "./components/TopBar"
 import RoomDetails from "./components/RoomDetails"
 import JoinRoom from "./components/JoinRoom"
+import Clients from "./components/Clients"
+import Footer from "./components/Footer"
 function App() {
 
   return (
-    <div className="App">
-      <TopBar />
-      <div className="mt-6 px-3 md:px-6 grid grid-col-1 md:grid-cols-2 gap-2">
-        <RoomDetails></RoomDetails>
-        <JoinRoom></JoinRoom>
+    <div className="App h-screen flex flex-col justify-between">
+      <div>
+        <TopBar />
+        <div className="mt-6 px-3 md:px-6 grid grid-col-1 md:grid-cols-2 gap-3">
+          <RoomDetails></RoomDetails>
+          <JoinRoom></JoinRoom>
+        </div>
+        <div className="px-3 md:px-6  grid grid-cols-1 my-3">
+          <Clients></Clients>
+        </div>
       </div>
+      <Footer></Footer>
     </div>
   )
 }
---
+++ client/src/components/Clients.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Clients() {
+    return (
+        <div className='w-full h-64 bg-gray-800 rounded-sm px-2'>
+            <div className='px-3 py-3 text-gray-200'>
+                <p className='font-mono text-xs text-gray-200'>All joined clients will appear here...</p>
+            </div>
+        </div>
+    )
+}
